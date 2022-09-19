e1036721cf049505191c2e2d8ae68bffcf26729a
Dmitry Gruzdev <degruzdev@gmail.com>
2022-09-19 20:07:53 +0300

    Delete script_ffmpeg.sh

--- script_ffmpeg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-path_ffmpeg = "/opt/qligent/vision/CurrentRelease/ffmpeg"
-while read LINE; do
-uri = $(echo $line | cut -f 3 -d"|")
-name = $(echo $line | cut -f 2 -d"|")
-$path_ffmpeg -i $uri -bsf:a aac_adtstoasc -t 10 -c copy $name
-done < list_monitoring_object
